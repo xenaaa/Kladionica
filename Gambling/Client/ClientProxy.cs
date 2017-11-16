0c1920f7457d97b2c629e303b3992406376297ad
Marina Bobar <marina.bobar@hotmail.com>
2017-11-16 09:11:01 +0100

    main programi

---
+++ Gambling/Client/ClientProxy.cs
@@ -0,0 +1,22 @@
+﻿using BetServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class ClientProxy : ChannelFactory<IBetService>, IBetService, IDisposable
+    {
+        IBetService factory;
+
+        public ClientProxy(NetTcpBinding binding, string address) : base(binding, address)
+        {
+            factory = this.CreateChannel();
+        }
+
+   
+    }
+}
