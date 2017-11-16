a3ff6a64fd5e16ec60f2caee11d7e72742ae22f8
Administrator <Administrator@P04-08>
2017-11-16 09:09:18 +0100

    IBetService

--- Gambling/IntegrationPlatform/IBetService.cs
+++ Gambling/IntegrationPlatform/IBetService.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace IntegrationPlatform
 {
+    [ServiceContract]
     public interface IBetService
     {
+        [OperationContract]
+        bool AddUser(User user);
+
+        [OperationContract]
+        bool DeleteUser(User user);
+
+        [OperationContract]
+        bool EditUser(User user);
+
+        [OperationContract]
+        bool SendGameResults(List<string> results);
+
+        [OperationContract]
+        bool SendOffers(List<BetOffer> offers);
+
+        [OperationContract]
+        bool SendTicket(Ticket ticket, string username);
+
+        [OperationContract]
+        bool SendTicketResults();
     }
 }
